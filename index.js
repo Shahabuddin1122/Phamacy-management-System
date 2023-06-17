866814361c96f931707e0b0c98f78c83b23278f3
Shahabuddin1122 <shavoddin54@gmail.com>
2023-06-17 12:50:51 +0600

    Doctor Registration done

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ app.post("/",enc,(req,res)=>{
             });
     
             const result=await connection.execute(`SELECT * FROM pharmacy_admin.Login where login_ID='${username}' and password='${password}'`);
+            console.log(result.rows);
             return result.rows;
         } catch (error) {
             return error;
@@ -49,7 +50,7 @@ app.post("/",enc,(req,res)=>{
     var password=req.body.password;
     fetchDataCustomer(username,password).
     then(dbRes=>{
-        // console.log(dbRes);
+        console.log(dbRes);
         if(dbRes.length>0){
             
             res.redirect("/Main2");
@@ -173,7 +174,44 @@ app.get('/appointment', async (req, res) => {
     await fetchDataCustomer(query);
   });
   
-  
+app.post('/addConsult', async (req, res) => {
+    const { Doc_id, Doc_Hos,Patient_id } = req.body;
+
+    let connection;
+
+    try {
+    connection = await oracledb.getConnection({
+        user: 'pharmacy_admin',
+        password: '12345',
+        connectString: 'localhost/xepdb1'
+    });
+
+    const insertQuery = `INSERT INTO Consults(Patient_id,Doctor_id)
+                        VALUES (:Patient_id,:Doc_id)`;
+
+    const bindParams = {
+        Patient_id,
+        Doc_id
+    };
+
+    const result = await connection.execute(insertQuery, bindParams);
+
+    
+    
+    } catch (error) {
+    // Handle error
+    res.status(500).json({ error: 'An error occurred while adding the pharmacy to the database.' });
+    console.error(error);
+    } finally {
+    if (connection) {
+        try {
+        await connection.close();
+        } catch (error) {
+        console.error(error);
+        }
+    }
+    }
+});
 
 
 
@@ -253,10 +291,89 @@ app.get('/search', async (req, res) => {
 
 
 
+let GLOBAL_ID='';
 
+app.post("/regi", enc, async (req, res) => {
+    async function fetchDataCustomer(un, email, house, road, city, district, pass) {
+      try {
+        const connection = await oracledb.getConnection({
+          user: 'pharmacy_admin',
+          password: '12345',
+          connectString: 'localhost/xepdb1'
+        });
+  
+        const result = await connection.execute(
+          `BEGIN
+             :new_login_id := '';
+             INSERT INTO login (password,USER_TYPE)
+             VALUES (:pass,'DOCTOR')
+             RETURNING login_id INTO :new_login_id;
+  
+             INSERT INTO doctor (doctor_id, doctor_name, doctor_email,doctor_address)
+             VALUES (:new_login_id, :un, :email,addr(:road,:city,:house,:district));
+  
+             :message := 'Records inserted successfully';
+          END;`,
+          {
+            new_login_id: { type: oracledb.STRING, dir: oracledb.BIND_OUT },
+            pass: pass,
+            un: un,
+            email: email,
+            road: road,
+            city: city,
+            house: house,
+            district: district,
+            message: { type: oracledb.STRING, dir: oracledb.BIND_OUT }
+          }
+        );
+        
+        await connection.commit();
+        await connection.close();
+        GLOBAL_ID=result.outBinds.new_login_id;
+        console.log('Generated Login ID:', result.outBinds.new_login_id);
+        
+        console.log(result.outBinds.message);
+        return result;
+      } catch (error) {
+        return error;
+      }
+    }
+  
+    let un = req.body.username;
+    let email = req.body.email;
+    let house = req.body.House_No;
+    let road = req.body.Road_NO;
+    let city = req.body.City;
+    let district = req.body.District;
+    let pass = req.body.password1;
+    fetchDataCustomer(un, email, house, road, city, district, pass)
+      .then(dbRes => {
 
-app.post("/regi",enc,(req,res)=>{
-    async function fetchDataCustomer(id,un,email,house,road,city,district,pass){
+        console.log(dbRes);
+        //res.redirect("/");
+        console.log('AT POST:', GLOBAL_ID);
+        res.redirect("/newlog");
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect("/regi");
+      });
+  });
+  
+app.get("/regi",(req,res)=>{
+    console.log('AT regi GET:', GLOBAL_ID);
+    res.sendFile(__dirname+"/regi.html");
+    
+    //res.render('regi',{data:GLOBAL_ID});
+    
+})
+app.get("/newlog",(req,res)=>{
+
+    res.render('newlog',{data:GLOBAL_ID});
+})
+
+app.post("/newlog",enc,(req,res)=>{
+    async function fetchDataCustomer(username,password){
         try {
             const connection=await oracledb.getConnection({
                 user:'pharmacy_admin',
@@ -264,59 +381,36 @@ app.post("/regi",enc,(req,res)=>{
                 connectionString: 'localhost/xepdb1'
             });
     
-            // const result=await connection.execute(`INSERT INTO DOCTOR (doctor_id,doctor_name,doctor_email,doctor_address) values('${id}','${un}','${email}',addr('${road}','${city}','${house}','${district}'))`);
-            // await connection.execute(`INSERT INTO LOGIN VALUES('${id}','${pass}');`);
-            // await connection.execute(`commit;`);
-            const query=`INSERT INTO DOCTOR (doctor_id,doctor_name,doctor_email,doctor_address) values(:1,:2,:3,addr(:4,:5,:6,:7))`;
-            const param={
-                1:id,
-                2:un,
-                3:email,
-                4:road,
-                5:city,
-                6:house,
-                7:district
-            }
-            const result=await connection.execute(query,param);
-            await connection.commit();
-            await connection.execute(`insert into login values(:1,:2)`,[id,pass]);
-            await connection.commit();
-            await connection.close();
-            return result;
+            const result=await connection.execute(`SELECT * FROM pharmacy_admin.Login where login_ID='${username}' and password='${password}'`);
+            console.log(result.rows);
+            return result.rows;
         } catch (error) {
             return error;
         }
-        
     }
-    let id="Doctor_003";
-    let un=req.body.username;
-    let email=req.body.email;
-    let house=req.body.house;
-    let road=req.body.road;
-    let city=req.body.City;
-    let district=req.body.District;
-    let pass=req.body.password1;
-    fetchDataCustomer(id,un,email,house,road,city,district,pass)
-        .then(dbRes=>{
-            // if(dbRes){
-            //     res.redirect("/");
-            // }
-            // else{
-            //     res.redirect("/regi");
-            // }
-            // console.log(dbRes);
-            res.redirect("/");
-        })
-        .catch(err=>{
-            res.redirect("/regi");
-            console.log(err);
-        })
+    var username=req.body.username;
+    var password=req.body.password;
+    fetchDataCustomer(username,password).
+    then(dbRes=>{
+        console.log(dbRes);
+        if(dbRes.length>0){
             
-        
+            res.redirect("/Main2");
+        }
+        else{
+            res.redirect("/");
+        }
+    })
+    .catch(err=>{
+        // console.log(err);
+        res.redirect("/");
+    })
 })
-app.get("/regi",(req,res)=>{
-    res.sendFile(__dirname+"/regi.html");
-})
+
+
+
+
+
 app.post("/forgot_pass",enc,(req,res)=>{
     async function fetchDataCustomer(un,pass1,pass2){
         try {
